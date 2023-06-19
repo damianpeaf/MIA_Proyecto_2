653af2764a96be2b9cf38f40f0ead0336b548190
Damian <damianpeaf@gmail.com>
2023-06-19 13:41:26 -0600

    ci:

--- kill-process.sh
+++
@@ -1,8 +0,0 @@
-# Obtener el ID del proceso asociado al puerto 8000
-OLD_PID=$(lsof -t -i :8000)
-
-# Matar el proceso anterior si existe
-if [[ -n $OLD_PID ]]; then
-    echo "Matando el proceso anterior en el puerto 8000: $OLD_PID"
-    kill $OLD_PID
-fi

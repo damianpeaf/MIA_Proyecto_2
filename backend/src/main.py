ae0769d6ff924c0ba7767778a8423f502379b18e
Damian <damianpeaf@gmail.com>
2023-06-20 16:23:02 -0600

    feat(backend): :bug: command request schema

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,26 @@
 
 from fastapi import FastAPI, Request
-
+from pydantic import BaseModel
 from commands import CommandProxy
+import datetime
 
 app = FastAPI()
 
 
+class CommandRequest(BaseModel):
+    command: str
+
 @app.post("/command")
-async def command(request: Request):
+async def command(request: CommandRequest):
 
-    body = await request.json()
-    command = body.get('command', '')
-    proxy = CommandProxy()
-    proxy.execute(command)
-
-    return proxy.response.get_json()
-
+    try:
+        command_line = request.command
+        proxy = CommandProxy()
+        proxy.execute(command_line)
+        return proxy.response.get_json()
+    except NotImplementedError as e:
+        return {"error": str(e)}
+    
 @app.get("/health")
 def health() -> dict:
     return {"status": "ok"}

150a5866fc5fb4f9dd27bbfb5b1d9a6ff3f15225
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-06-27 20:18:13 -0600

    feat(backend): validate use .txt for users from S3

--- backend/src/auth/validate_user.py
+++ backend/src/auth/validate_user.py
@@ -2,9 +2,13 @@ from os import path, getcwd
 
 from utils import aes_encryption, aes_decryption
 from commands import CommandResponse, IOType
+from commands.service.own_bucket import OwnBucketService
 
-LOCAL_BASE_PATH = path.abspath(getcwd())
-USERS_FILE = 'miausuarios.txt'
+base_file = OwnBucketService()
+base_file.on_root = True
+
+# LOCAL_BASE_PATH = base_file.get_file('/miausuarios.txt')
+# USERS_FILE = 'miausuarios.txt'
 AES_KEY = 'miaproyecto12345'
 
 """
@@ -25,48 +29,50 @@ def validate_user(username: str = '', password: str = '') -> dict:
 
     response.info('Intento de inicio de sesión del usuario: ' + username, IOType.AUTH)
 
-    if not path.exists(path.join(LOCAL_BASE_PATH, USERS_FILE)):
-        response.error('El archivo de usuarios no existe.', IOType.AUTH)
-        return {
-            'username': username,
-            'ok': False,
-            'response': response
-        }
+    # if not path.exists(path.join(LOCAL_BASE_PATH, USERS_FILE)):
+    #     response.error('El archivo de usuarios no existe.', IOType.AUTH)
+    #     return {
+    #         'username': username,
+    #         'ok': False,
+    #         'response': response
+    #     }
 
-    with open(path.join(LOCAL_BASE_PATH, USERS_FILE), 'r') as users_file:
-        lines = users_file.readlines()
+    # with open(path.join(LOCAL_BASE_PATH, USERS_FILE), 'r') as users_file:
+    #     lines = users_file.readlines()
 
-        username_found = False
-        check = True  # true for username, false for password
+    lines: list[str] = base_file.get_file('/miausuarios.txt')['file_content'].split('\n')
 
-        for line in lines:
+    username_found = False
+    check = True  # true for username, false for password
 
-            # Skip empty lines
-            if line.strip() == '':
-                continue
+    for line in lines:
 
-            # If the username was found, check the password
-            if username_found:
-                if line.strip().upper() == aes_encryption(AES_KEY, password).upper():
-                    response.success('Inicio de sesión exitoso del usuario: ' + username, IOType.AUTH)
-                    return {
-                        'username': username,
-                        'ok': True,
-                        'response': response
-                    }
+        # Skip empty lines
+        if line.strip() == '':
+            continue
 
-                # If the password was not found, return False
-                response.error('Contraseña incorrecta para el usuario: ' + username, IOType.AUTH)
+        # If the username was found, check the password
+        if username_found:
+            if line.strip().upper() == aes_encryption(AES_KEY, password).upper():
+                response.success('Inicio de sesión exitoso del usuario: ' + username, IOType.AUTH)
                 return {
                     'username': username,
-                    'ok': False,
+                    'ok': True,
                     'response': response
                 }
-            elif check:
-                if line.strip() == username.strip():
-                    username_found = True
 
-            check = not check
+            # If the password was not found, return False
+            response.error('Contraseña incorrecta para el usuario: ' + username, IOType.AUTH)
+            return {
+                'username': username,
+                'ok': False,
+                'response': response
+            }
+        elif check:
+            if line.strip() == username.strip():
+                username_found = True
+
+        check = not check
 
     response.error('El usuario no existe: ' + username, IOType.AUTH)
     return {
